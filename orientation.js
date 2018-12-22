43b1899b480f0ad832f46a40aa6202e009c170c7
Teffen Ellis <teffen@nirri.us>
2018-12-21 19:14:03 -0500

    Revert "Change axis."
    This reverts commit 96302ed4980cb86da9d2d76a9af996434b308400.

--- orientation.js
+++ orientation.js
@@ -59,7 +59,7 @@ function handleDeviceOrientation (event) {
   let vertical
   if (verticalChanged) {
     // if (gamma > 0)
-    vertical = gamma > previousPosition.gamma ? 'down' : 'up'
+    vertical = beta > previousPosition.beta ? 'down' : 'up'
   } else {
     vertical = previousPosition.vertical
   }
